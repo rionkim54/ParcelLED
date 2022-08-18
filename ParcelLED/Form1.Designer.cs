85b5b1613baea23a7e2b2705563064ba63c5416f
ZEROWIN <cherrychance@naver.com>
2022-08-18 13:39:18 +0900

    init

---
+++ ParcelLED/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace ParcelLED
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.tbWinXY = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.CausesValidation = false;
+            this.textBox1.Font = new System.Drawing.Font("굴림", 8.25F);
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(10, 10);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBox1.Size = new System.Drawing.Size(60, 40);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.CausesValidation = false;
+            this.textBox2.Font = new System.Drawing.Font("굴림", 8.25F);
+            this.textBox2.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.textBox2.Location = new System.Drawing.Point(70, 10);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(0);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox2.Size = new System.Drawing.Size(60, 40);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "도착날짜\r\n7월 4일\r\n";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.CausesValidation = false;
+            this.textBox3.Font = new System.Drawing.Font("굴림", 8.25F);
+            this.textBox3.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.textBox3.Location = new System.Drawing.Point(10, 50);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(0);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(60, 40);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "보관시간\r\n23:30";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.CausesValidation = false;
+            this.textBox4.Font = new System.Drawing.Font("굴림", 8.25F);
+            this.textBox4.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.textBox4.Location = new System.Drawing.Point(70, 50);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(0);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox4.Size = new System.Drawing.Size(60, 40);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.Text = "남은시간";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // tbWinXY
+            // 
+            this.tbWinXY.Location = new System.Drawing.Point(70, 213);
+            this.tbWinXY.Name = "tbWinXY";
+            this.tbWinXY.Size = new System.Drawing.Size(100, 20);
+            this.tbWinXY.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 11F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(914, 413);
+            this.Controls.Add(this.tbWinXY);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("굴림", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.ForeColor = System.Drawing.Color.MediumBlue;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseLeave += new System.EventHandler(this.Form1_MouseLeave);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.TextBox tbWinXY;
+    }
+}
+
---
+++ ParcelLED/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace ParcelLED
+{
+    public partial class Form1 : Form
+    {
+        public static string server_ip = "192.168.0.2";
+        public static int port = 502;
+        public static int data_addresss = 40000;
+
+
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            Thread thread1 = new Thread(connect);  // Thread 객채 생성, Form과는 별도 쓰레드에서 connect 함수가 실행됨.
+            thread1.IsBackground = true;  // Form이 종료되면 thread1도 종료.
+            thread1.Start();  // thread1 시작.
+
+            timer2.Start();
+
+            this.Location = Properties.Settings.Default.Location;
+
+            tbWinXY.Text = this.Location.X + "," + this.Location.Y;
+
+            this.BringToFront();
+            this.Focus();
+            this.KeyPreview = true;
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+
+
+            //textBox1.Font = new Font(textBox1.Font.FontFamily, 8);
+            //textBox1.Text = "택배도착날짜\r\n12월31일\r\n화요일";
+
+            textBox1.Font = new Font(textBox1.Font.FontFamily, 8);
+            textBox1.Text = "남은\r\n보관시간\r\n23:30";
+            textBox1.ForeColor = Color.Orange;
+
+        }
+
+        public static TcpClient client = new TcpClient();
+        public static NetworkStream stream;
+
+        public static string ByteArrayToString(byte[] ba)
+        {
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            foreach (byte b in ba)
+                hex.AppendFormat("{0:x2}", b);
+            return hex.ToString();
+        }
+
+        private void connect()  // thread1에 연결된 함수. 메인폼과는 별도로 동작한다.
+        {
+            //TcpClient tc = new TcpClient(server_ip, 502);
+            ////TcpClient tc = new TcpClient("localhost", 7000);
+
+            ////string msg = "Hello World";
+            ////byte[] buff = Encoding.ASCII.GetBytes(msg);
+            //Byte[] sendbuf = new byte[] { 0, 6, 0, 0, 0, 6, 1, 1, 0x9c, 0x40, 0, 0x10 };
+
+
+            //// (2) NetworkStream을 얻어옴 
+            //NetworkStream stream = tc.GetStream();
+
+            //// (3) 스트림에 바이트 데이타 전송
+            //stream.Write(sendbuf, 0, sendbuf.Length);
+
+            //// (4) 스트림으로부터 바이트 데이타 읽기
+            //byte[] outbuf = new byte[1024];
+            //int nbytes = stream.Read(outbuf, 0, outbuf.Length);
+            //string output = Encoding.ASCII.GetString(outbuf, 0, nbytes);
+
+            //// (5) 스트림과 TcpClient 객체 닫기
+            //stream.Close();
+            //tc.Close();
+
+            //Console.WriteLine($"{nbytes} bytes: {output}");
+
+            try
+            {
+                IPEndPoint clientAddress = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 55555);
+                IPEndPoint serverAddress = new IPEndPoint(IPAddress.Parse(server_ip), port);
+
+                Console.WriteLine("클라이언트: {0}, 서버: {1}", clientAddress.ToString(), serverAddress.ToString());
+
+                client.Connect(serverAddress);
+
+                //byte[] data = Encoding.Default.GetBytes(message);
+                stream = client.GetStream();
+
+
+                //Byte[] sendbuf = new byte[] { 0, 0, 0, 0, 0, 6, 0, 3, 0x9c, 0x40, 0, 0x0a };
+                //stream.Write(sendbuf, 0, sendbuf.Length);
+
+                //Console.WriteLine("송신: {0}", sendbuf);
+
+                Byte [] data = new byte[256];
+
+                string responseData = "";
+
+                while (stream.CanRead)
+                {
+                    byte [] myReadBuffer = new byte[1024];
+                    String myCompleteMessage = "";
+                    int numberOfBytesRead = 0;
+                   
+
+                    // Incoming message may be larger than the buffer size.
+                    do
+                    {
+                        numberOfBytesRead = stream.Read(myReadBuffer, 0, myReadBuffer.Length);
+                        //myCompleteMessage =
+                        //    String.Concat(myCompleteMessage, Encoding.ASCII.GetString(myReadBuffer, 0, numberOfBytesRead));
+
+                        byte[] byteBuffer = new byte[numberOfBytesRead]; //  myReadBuffer.Length];
+
+                        Array.Copy(myReadBuffer, 0, byteBuffer, 0, numberOfBytesRead); //  myReadBuffer.Length);
+
+                        myCompleteMessage = ByteArrayToString(byteBuffer);
+
+
+                    }
+                    while (stream.DataAvailable);
+
+                    Console.WriteLine(myCompleteMessage);
+                }
+                //else
+                //{
+                //    Console.WriteLine("Sorry.  You cannot read from this NetworkStream.");
+                //}
+
+                stream.Close();
+                client.Close();
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private void writeRichTextbox(string data)  // richTextbox1 에 쓰기 함수
+        {
+            System.Console.WriteLine(data);
+        }
+
+        private int xpos = 0, ypos = 0;
+        public string mode = "Left-to-Right";
+
+
+
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (stream == null) return;
+            //NetworkStream stream = tc.GetStream();
+            Byte[] sendbuf = new byte[] { 0, 0, 0, 0, 0, 6, 0, 3, 0, 0, 0, 0x0a };
+            stream.Write(sendbuf, 0, sendbuf.Length);
+
+            Console.WriteLine("송신: {0}", sendbuf);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // (5) 스트림과 TcpClient 객체 닫기
+            Properties.Settings.Default.Location = this.Location;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                this.Location = new Point(this.Location.X - 1, this.Location.Y);
+                Properties.Settings.Default.Location = this.Location;
+                Properties.Settings.Default.Save();
+                tbWinXY.Text = this.Location.X + "," + this.Location.Y;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                this.Location = new Point(this.Location.X + 1, this.Location.Y);
+                Properties.Settings.Default.Location = this.Location;
+                Properties.Settings.Default.Save();
+                tbWinXY.Text = this.Location.X + "," + this.Location.Y;
+
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                this.Location = new Point(this.Location.X, this.Location.Y-1);
+                Properties.Settings.Default.Location = this.Location;
+                Properties.Settings.Default.Save();
+                tbWinXY.Text = this.Location.X + "," + this.Location.Y;
+
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                this.Location = new Point(this.Location.X, this.Location.Y + 1);
+                Properties.Settings.Default.Location = this.Location;
+                Properties.Settings.Default.Save();
+                tbWinXY.Text = this.Location.X + "," + this.Location.Y;
+
+            }
+
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseLeave(object sender, EventArgs e)
+        {
+            this.Location = new Point(this.Location.X, this.Location.Y + 1);
+            Properties.Settings.Default.Location = this.Location;
+            Properties.Settings.Default.Save();
+            tbWinXY.Text = this.Location.X + "," + this.Location.Y;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+        }
+    }
+}
